9a2df0fc393cbc57e79ca9644702c967f414b6c9
Guilherme Gomes <gguife747@gmail.com>
2023-08-27 21:07:13 -0300

    resolvendo desafio Operacoes basicas com List

---
+++ src/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package OperacoesBasicas;
+
+public class Tarefa {
+  //Atributo
+  private String descricao;
+
+  //Construtor
+  public Tarefa(String descricao){
+    this.descricao = descricao;
+  }
+
+  //Getter
+  public String getDescricao(){
+    return this.descricao;
+  }
+
+  @Override
+  public String toString(){
+    return descricao; 
+  }
+}
